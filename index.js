6be816133000a68cd7d1da016288041d4d09c944
gowtham2k2 <gowthamangowda7@gmail.com>
2024-04-21 17:06:37 +0530

    fix: minified, removed unwanted code

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
-var mainHtml = document.getElementsByTagName("html")[0];
 
-if (mainHtml.clientWidth < 680) {
-  let details = document.querySelectorAll("#details .details-grid>*");
-
-  details.forEach((element) => {
-    if (element.classList.contains("interest")) {
-      element.style.cssText = "order:-2";
-    }
-    if (element.classList.contains("keySkills")) {
-      element.style.cssText = "order:-1";
-    }
-  });
-}
+// hover and copy clipboard event
 
 var contactElements = document.querySelectorAll("#intro .contacts>*");
 
 contactElements.forEach((element) => {
-  element.addEventListener("mouseover", () => {
-    element.classList.add("contactHover");
-  });
-  element.addEventListener("mouseout", () => {
-    element.classList.remove("contactHover");
-  });
-  if (element.nodeName != "A") {
-    element.addEventListener("click", () => {
-      navigator.clipboard.writeText(element.firstElementChild.textContent);
-      alert("text copied");
+  $(element)
+    .on("mouseover", () => {
+      $(element).addClass("contactHover");
+    })
+    .on("mouseout", () => {
+      $(element).removeClass("contactHover");
+    })
+    .on("click", () => {
+      if ($(element)[0].nodeName != "A") {
+        navigator.clipboard.writeText(element.firstElementChild.textContent);
+        alert("text copied");
+      }
     });
-  }
 });
